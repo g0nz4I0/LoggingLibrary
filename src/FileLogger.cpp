9aa5c3adee180316a4cafc09ce845dbf0d6984d5
Gonzalo Sanchez <gonzalosmoya24@gmail.com>
2024-08-01 12:31:01 +0200

    std::terminate() if file hasnt opened

--- src/FileLogger.cpp
+++ src/FileLogger.cpp
@@ -12,6 +12,9 @@ FileLogger::FileLogger(fs::path path_to_log_file)
 		std::cout << std::format("Log file {} already exists, trucating ...",path_to_log_file.generic_string());
 	}
 	log_file = std::ofstream{ path_to_log_file, log_file.trunc | log_file.in };
+	if (!log_file.is_open()) {
+		throw std::runtime_error(std::format("File {} could not be opened, check directory permmissions"));
+	}
 }
 void FileLogger::write(std::string& msg) {
 
